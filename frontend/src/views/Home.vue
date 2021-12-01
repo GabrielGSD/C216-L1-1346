9edf3c5516b2a6c4f0c50e63f8ac893229600797
projetos_b2ml <gabrieldaniel@gec.inatel.br>
2021-11-30 23:41:52 -0300

    Start layout

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 class="title">C216 - Produtos</h1>
+
+    <button class="btnAdd">+</button>
+
+    <div class="boxScroll">
+      <p>vrau</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
